2f1800c8a02710fe046406c93f603b631ddd3c44
miladiashe <dkdud0122@naver.com>
2025-09-02 16:27:15 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-
-
 export default function Home() {
-
-  
   return (
-    <div className="flex items-center justify-center h-screen align-middle flex-col">
-      황크
+    <div className="flex items-center justify-center align-middle flex-row gap-4">
+      <button className="border-2 border-gray-600 bg-gray-600 hover:border-white text-white py-2 px-4 rounded" >황크</button>
+      <button className="border-2 border-gray-600 bg-gray-600 hover:border-white text-white py-2 px-4 rounded" >어사</button>  
     </div>
   );
 }
